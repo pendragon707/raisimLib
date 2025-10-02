807d02be5394059c7304c89044f4e8bb2306a348
Nonepenguin <pendragon707@mail.ru>
2025-10-02 11:10:20 +0300

    update readme and docker

--- docker/rma_entry.sh
+++ docker/rma_entry.sh
@@ -2,4 +2,8 @@
 
 source /root/miniconda3/bin/activate
 conda activate rma
+
+pip install cbor2 mujoco crcmod
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+
 cd /workspace
